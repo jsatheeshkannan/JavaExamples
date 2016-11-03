6b349824686fdd041db0c759ce27422c14ee45f6
Satheesh Kannan (c) <kannans@vmware.com>
2016-11-03 07:50:33 +0530

    try to avoid picture using gitignore

--- src/main/java/org/git/testng/TestNGExtend.java
+++ src/main/java/org/git/testng/TestNGExtend.java
@@ -1,5 +1,6 @@
 package org.git.testng;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestNGExtend extends TestngSupport{
@@ -8,5 +9,8 @@ public class TestNGExtend extends TestngSupport{
 	public void extendTest1(){
 		System.out.println("$$$$extended Test1 with Test Annotation$$$$");
 	}
-
+	@BeforeTest
+	public void extendBeforeTest(){
+		System.out.println("$$$$extend Before Test with BeforeTest Annotation");
+	}
 }
